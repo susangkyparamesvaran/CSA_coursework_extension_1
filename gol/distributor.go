e9ecca5f824650556f1b49818ac3de338b615dba
SUSANGKY <hi24376@lol.uk>
2025-11-13 07:01:24 +0000

    hopefully should workkk

--- gol/distributor.go
+++ gol/distributor.go
@@ -1,5 +1,14 @@
 package gol
 
+import (
+	"fmt"
+	"net/rpc"
+	"os"
+	"time"
+
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -9,24 +18,338 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
+// worker request is send from the controller to each AWS node
+// will contain the full world and the row range
+type WorkerRequest struct {
+	Params Params
+	World  [][]byte
+	StartY int
+	EndY   int
+}
+
+// worker response is what the AWS node returns
+type WorkerResponse struct {
+	StartY  int
+	Section [][]byte
+	Alive   []util.Cell
+}
+
+// for which rows of the world belong to an AWS node
+type nodeSection struct {
+	start int
+	end   int
+}
+
 // distributor divides the work between workers and interacts with other goroutines.
-func distributor(p Params, c distributorChannels) {
+func distributor(p Params, c distributorChannels, keypress <-chan rune) {
 
 	// TODO: Create a 2D slice to store the world.
 
+	filename := fmt.Sprintf("%dx%d", p.ImageWidth, p.ImageHeight)
+	c.ioCommand <- ioInput
+	c.ioFilename <- filename
+
+	world := make([][]byte, p.ImageHeight)
+	for y := 0; y < p.ImageHeight; y++ {
+		world[y] = make([]byte, p.ImageWidth)
+		for x := 0; x < p.ImageWidth; x++ {
+			world[y][x] = <-c.ioInput
+		}
+	}
+
+	// have a list of the AWS nodes addresses
+	workerAddresses := []string{
+		//worker 1
+		//worker 2
+		//worker 3
+	}
+
+	numWorkers := len(workerAddresses)
+
+	// check if any workers have been dialled
+	if numWorkers == 0 {
+		fmt.Println("No worker addresses dialled")
+		return
+	}
+
+	// dial all the workers and store the rpc clients
+	clients := make([]*rpc.Client, numWorkers)
+	for i, address := range workerAddresses {
+		client, err := rpc.Dial("tcp", address)
+		if err != nil {
+			fmt.Println("Error connecting to worker", address, ":", err)
+			return
+		}
+		clients[i] = client
+	}
+
+	// ensure that all the rpc connections are closed when distributor exits
+	defer func() {
+		for _, client := range clients {
+			client.Close()
+		}
+	}()
+
+	// Start ticker to report alive cells every 2 seconds
+	ticker := time.NewTicker(2 * time.Second)
+	//Channel used to sognal the goroutine to stop
+	done := make(chan bool)
+
 	turn := 0
+
+	go func() {
+		for {
+			select {
+			// Case runs every time the timer ticks (every 2 seconds)
+			case <-ticker.C:
+				totalAlive := 0
+				for _, client := range clients {
+					var aliveCount int
+
+					err := client.Call("GOLWorker.GetAliveCount", struct{}{}, &aliveCount)
+
+					if err != nil {
+						fmt.Println("Error calling GetAliveCount:", err)
+						continue
+					}
+
+					totalAlive = totalAlive + aliveCount
+				}
+
+				c.events <- AliveCellsCount{
+					CompletedTurns: turn,
+					CellsCount:     totalAlive,
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	// calculates which cells are alive in the inital state before a turn has been made
+	initialAlive := AliveCells(world, p.ImageWidth, p.ImageHeight)
+	if len(initialAlive) > 0 {
+		c.events <- CellsFlipped{
+			CompletedTurns: 0,
+			Cells:          initialAlive}
+	}
+
 	c.events <- StateChange{turn, Executing}
 
+	// for each turn it needs to split up the jobs,
+	// such that there is one job from eahc section for each thread
+	// needs to gather the results and then put them together for the newstate of world
 	// TODO: Execute all turns of the Game of Life.
 
+	//----------------------------------------------------------------------------------------------------------//
+	//----------------------------------------------------------------------------------------------------------//
+
+	// variables for step 5
+	paused := false
+	quitting := false
+
+	for {
+		select {
+		case key := <-keypress:
+			switch key {
+			case 'p':
+				if !paused {
+					paused = true
+					fmt.Println("paused at turn:", turn)
+					c.events <- StateChange{turn, Paused}
+				} else {
+					paused = false
+					fmt.Println("continuing")
+					c.events <- StateChange{turn, Executing}
+				}
+			case 's':
+				saveImage(p, c, world, turn)
+			case 'q':
+				quitting = true
+				fmt.Println("quitting, sending signal to worker")
+				for _, client := range clients {
+					_ = client.Call("GOLWorker.Shutdown", struct{}{}, &struct{}{})
+				}
+			case 'k':
+				fmt.Println("shutting down full system")
+				for _, client := range clients {
+					_ = client.Call("GOLWorker.Shutdown", struct{}{}, &struct{}{})
+				}
+				saveImage(p, c, world, turn)
+				c.events <- StateChange{turn, Quitting}
+				close(c.events)
+				os.Exit(0)
+			}
+			continue
+		default:
+		}
+
+		// quit if acc finished and not paused
+		if quitting || (turn >= p.Turns && !paused) {
+			break
+		}
+
+		if paused {
+			time.Sleep(10 * time.Millisecond)
+			continue
+		}
+
+		// split the rows across the AWS nodes
+		nodeSections := assignSections(p.ImageHeight, numWorkers)
+
+		// collect each node's response to build overall world
+		responses := make([]WorkerResponse, numWorkers)
+
+		// ask each AWS node to process its slice
+		for i, client := range clients {
+			section := nodeSections[i]
+			request := WorkerRequest{
+				Params: p,
+				World:  world,
+				StartY: section.start,
+				EndY:   section.end,
+			}
+
+			var response WorkerResponse
+
+			err := client.Call("GOLWorker.ProcessTurns", request, &response)
+			if err != nil {
+				fmt.Println("Error calling remote worker:", err)
+				quitting = true
+				continue
+			}
+
+			// store the response in correct index
+			responses[i] = response
+		}
+
+		// assemble a new world
+		newWorld := make([][]byte, p.ImageHeight)
+		for y := 0; y < p.ImageHeight; y++ {
+			newWorld[y] = make([]byte, p.ImageWidth)
+		}
+
+		for _, response := range responses {
+			start := response.StartY
+			for i, row := range response.Section {
+				newWorld[start+i] = row
+			}
+		}
+
+		///// STEP 6 CELLS FLIPPED///////////
+		// At the end of each turn, put all changed coordinates into a slice,
+		// and then send CellsFlipped event
+		// make a slice so as to compare the old row and the new row of the world
+		flippedCells := make([]util.Cell, 0)
+		// go row by row, then column by column
+		for y := 0; y < p.ImageHeight; y++ {
+			for x := 0; x < p.ImageWidth; x++ {
+				if world[y][x] != newWorld[y][x] {
+					flippedCells = append(flippedCells, util.Cell{X: x, Y: y})
+				}
+			}
+		}
+
+		// if there is at least one cell thats been flipped then we need to return the
+		// Cells Flipped event
+		if len(flippedCells) > 0 {
+			c.events <- CellsFlipped{
+				CompletedTurns: turn + 1,
+				Cells:          flippedCells}
+		}
+
+		world = newWorld
+
+		///// STEP 6 TURN COMPLETE///////////
+		// At the end of each turn we need to signal that a turn is completed
+		c.events <- TurnComplete{
+			CompletedTurns: turn + 1,
+		}
+
+		turn++ //advance turn
+	}
+
+	//Stop ticker after finishing all turns
+	done <- true
+	ticker.Stop()
+
 	// TODO: Report the final state using FinalTurnCompleteEvent.
+	aliveCells := AliveCells(world, p.ImageWidth, p.ImageHeight)
+	c.events <- FinalTurnComplete{CompletedTurns: turn, Alive: aliveCells}
+
+	//save final output
+	saveImage(p, c, world, turn)
+	c.events <- StateChange{turn, Quitting}
+
+	//Close the channels to stop the SDL goroutine gracefully. Removing may cause deadlock.
+	close(c.events)
+
+}
+
+func AliveCells(world [][]byte, width, height int) []util.Cell {
+	cells := make([]util.Cell, 0)
+	for dy := 0; dy < height; dy++ {
+		for dx := 0; dx < width; dx++ {
+			if world[dy][dx] == 255 {
+				cells = append(cells, util.Cell{X: dx, Y: dy})
+			}
+		}
+	}
+	return cells
+}
+
+// helper function to handle image saves
+func saveImage(p Params, c distributorChannels, world [][]byte, turn int) {
+	// Write final world to output file (PGM)
+	// Construct the output filename in the required format
+	// Example: "512x512x100" for a 512x512 world after 100 turns
+	outFileName := fmt.Sprintf("%dx%dx%d", p.ImageWidth, p.ImageHeight, turn)
+	c.ioCommand <- ioOutput     // telling the i/o goroutine that we are starting an output operation
+	c.ioFilename <- outFileName // sending the filename to io goroutine
+
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			//writing the pixel value to the ioOutput channel
+			c.ioOutput <- world[y][x] //grayscale value for that pixel (0 or 255)
+		}
+	}
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
-	c.events <- StateChange{turn, Quitting}
+	// once saved, notify the SDL event system (important for Step 5)
+	c.events <- ImageOutputComplete{CompletedTurns: turn, Filename: outFileName}
 
-	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
-	close(c.events)
+}
+
+// helper function to split the rows of the image between the AWS nodes
+func assignSections(height, numNodes int) []nodeSection {
+
+	// we need to calculate the minimum number of rows for each worker
+	minRows := height / numNodes
+	// then say if we have extra rows left over then we need to assign those evenly to each worker
+	extraRows := height % numNodes
+
+	// make a slice, the size of the number of threads
+	sections := make([]nodeSection, numNodes)
+	start := 0
+
+	for i := 0; i < numNodes; i++ {
+		// assigns the base amount of rows to the thread
+		rows := minRows
+		// if say we're on worker 2 and there are 3 extra rows left,
+		// then we can add 1 more job to the thread
+		if i < extraRows {
+			rows++
+		}
+
+		// marks where the end of the section ends
+		end := start + rows
+		// assigns these rows to the section
+		sections[i] = nodeSection{start: start, end: end}
+		// start is updated for the next worker
+		start = end
+	}
+	return sections
 }
---
+++ worker/worker.go
@@ -0,0 +1,288 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"os"
+
+	"uk.ac.bris.cs/gameoflife/gol"
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
+type GOLWorker struct {
+	world  [][]byte
+	params gol.Params
+}
+
+type WorkerRequest struct {
+	Params gol.Params
+	World  [][]byte
+	StartY int
+	EndY   int
+}
+
+type WorkerResponse struct {
+	StartY  int
+	Section [][]byte
+	Alive   []util.Cell
+}
+
+// structs for our channels used to communicate with the worker goroutine
+type workerJob struct {
+	startY int
+	endY   int
+	world  [][]byte
+}
+
+type workerResult struct {
+	ID           int
+	startY       int
+	worldSection [][]byte
+}
+
+type section struct {
+	start int
+	end   int
+}
+
+func (e *GOLWorker) ProcessTurns(req WorkerRequest, res *WorkerResponse) error {
+	p := req.Params
+	world := req.World
+
+	// start and end row for aws node
+	startY := req.StartY
+	endY := req.EndY
+
+	rows := endY - startY
+
+	// Channels to send work and receive results
+	jobChan := make(chan workerJob)
+	resultChan := make(chan workerResult)
+
+	// split the sections node's rows into p.Threads
+	workerSections := assignRows(rows, p.Threads)
+
+	// call goroutine
+	for i := 0; i < p.Threads; i++ {
+		go worker(i, p, jobChan, resultChan)
+	}
+
+	for _, job := range workerSections {
+		jobChan <- workerJob{
+			// relative to node's section of rows
+			startY: job.start + startY,
+			endY:   job.end + startY,
+			world:  world,
+		}
+	}
+	close(jobChan)
+
+	// collect all the resuts and put them into the new state of world
+	results := make([]workerResult, 0, len(workerSections))
+	for i := 0; i < len(workerSections); i++ {
+		results = append(results, <-resultChan)
+	}
+	close(resultChan)
+
+	// new world state
+	newWorld := make([][]byte, p.ImageHeight)
+	for i := range newWorld {
+		newWorld[i] = make([]byte, p.ImageWidth)
+	}
+
+	// build correct new section of world
+	sectionRows := make([][]byte, rows)
+	for _, result := range results {
+		numRows := result.startY - startY
+		for i, row := range result.worldSection {
+			sectionRows[numRows+i] = row
+		}
+	}
+
+	// Populate response -> this is getting sent back to the local machine
+	res.StartY = startY
+	res.Section = sectionRows
+	res.Alive = gol.AliveCells(sectionRows, p.ImageWidth, rows)
+
+	//this is just updating the internal state, so server can keep track of the worlds state after a turn
+	//otherwise worker would forget what world its simulating after each turn
+	e.world = sectionRows
+	e.params = p
+
+	return nil
+}
+
+func calculateNextStates(p gol.Params, world [][]byte, startY, endY int) [][]byte {
+	h := p.ImageHeight //h rows
+	w := p.ImageWidth  //w columns
+
+	rows := endY - startY
+
+	//make new grid section
+	newRows := make([][]byte, rows)
+	for i := 0; i < rows; i++ {
+		newRows[i] = make([]byte, w)
+	}
+
+	for i := startY; i < endY; i++ {
+		for j := 0; j < w; j++ { //accessing each individual cell
+			count := 0
+			up := (i - 1 + h) % h
+			down := (i + 1) % h
+			left := (j - 1 + w) % w
+			right := (j + 1) % w
+
+			//need to check all it's neighbors and state of it's cell
+			leftCell := world[i][left]
+			if leftCell == 255 {
+				count += 1
+			}
+			rightCell := world[i][right]
+			if rightCell == 255 {
+				count += 1
+			}
+			upCell := world[up][j]
+			if upCell == 255 {
+				count += 1
+			}
+			downCell := world[down][j]
+			if downCell == 255 {
+				count += 1
+			}
+			upRightCell := world[up][right]
+			if upRightCell == 255 {
+				count += 1
+			}
+			upLeftCell := world[up][left]
+			if upLeftCell == 255 {
+				count += 1
+			}
+
+			downRightCell := world[down][right]
+			if downRightCell == 255 {
+				count += 1
+			}
+
+			downLeftCell := world[down][left]
+			if downLeftCell == 255 {
+				count += 1
+			}
+
+			//update the cells
+			if world[i][j] == 255 {
+				if count == 2 || count == 3 {
+					newRows[i-startY][j] = 255
+				} else {
+					newRows[i-startY][j] = 0
+				}
+			}
+
+			if world[i][j] == 0 {
+				if count == 3 {
+					newRows[i-startY][j] = 255
+				} else {
+					newRows[i-startY][j] = 0
+				}
+			}
+
+		}
+	}
+	return newRows
+}
+
+// worker goroutine
+func worker(id int, p gol.Params, jobs <-chan workerJob, results chan<- workerResult) {
+	for job := range jobs {
+		outputSection := calculateNextStates(p, job.world, job.startY, job.endY)
+		results <- workerResult{
+			ID:           id,
+			startY:       job.startY,
+			worldSection: outputSection,
+		}
+	}
+}
+
+// helper func to assign sections of image to workers based on no. of threads
+func assignRows(height, threads int) []section {
+	// say if we had 16 rows and 4 threads
+	// we want to be able to allocate say 4 rows to 1 thread, 4 to the other thread etc.
+	workers := threads
+
+	// we need to calculate the minimum number of rows for each worker
+	minRows := height / threads
+	// then say if we have extra rows left over then we need to assign those evenly to each worker
+	extraRows := height % threads
+
+	// make a slice, the size of the number of threads
+	sections := make([]section, workers)
+	start := 0
+
+	for i := 0; i < workers; i++ {
+		// assigns the base amount of rows to the thread
+		rows := minRows
+		// if say we're on worker 2 and there are 3 extra rows left,
+		// then we can add 1 more job to the thread
+		if i < extraRows {
+			rows++
+		}
+
+		// marks where the end of the section ends
+		end := start + rows
+		// assigns these rows to the section
+		sections[i] = section{start: start, end: end}
+		// start is updated for the next worker
+		start = end
+	}
+	return sections
+}
+
+// counting alive cells, doesn't acc need any parameters but cuz its gotta follow format hence _ struct{}
+func (e *GOLWorker) GetAliveCount(_ struct{}, res *int) error {
+	count := 0
+	for y := 0; y < len(e.world); y++ {
+		for x := 0; x < len(e.world[y]); x++ {
+			if e.world[y][x] == 255 {
+				count++
+			}
+		}
+	}
+	*res = count
+	return nil
+}
+
+// helper func to make worker shut down on keypress
+func (e *GOLWorker) Shutdown(_ struct{}, _ *struct{}) error {
+	fmt.Println("shutdown signal recieved, stopping worker.")
+
+	go func() {
+		os.Exit(0)
+	}()
+	return nil
+}
+
+func main() {
+	err := rpc.Register(new(GOLWorker))
+	if err != nil {
+		fmt.Println("Error registering RPC:", err)
+		return
+	}
+
+	listener, err := net.Listen("tcp4", "0.0.0.0:8030")
+	if err != nil {
+		fmt.Println("Error starting listener:", err)
+		return
+	}
+	fmt.Println("Worker listening on port 8030 (IPv4)...")
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Connection error:", err)
+			continue
+		}
+		go rpc.ServeConn(conn)
+	}
+}
