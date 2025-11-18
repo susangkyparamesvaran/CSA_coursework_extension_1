ffa61549ced2b2e9f94256a276c61af60810a45f
Nasywa  Sakimun <tt24385@bristol.ac.uk>
2025-11-18 07:30:35 +0000

    Update distributor.go

--- gol/distributor.go
+++ gol/distributor.go
@@ -61,7 +61,7 @@ func distributor(p Params, c distributorChannels, keypress <-chan rune) {
 	// have a list of the AWS nodes addresses
 	workerAddresses := []string{
 		"3.235.67.169:8030",
-		//"3.83.185.159:8030",
+		"3.83.185.159:8030",
 		//"98.93.181.139:8030",
 		//"3.237.1.5:8030",
 	}
